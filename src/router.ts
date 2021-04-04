78d22b40140071c4d41320884ccb7dc56eb49482
joseluisgs <joseluisgonsan@gmail.com>
2021-04-04 10:18:04 +0200

    Enpoint API Version Hola

--- src/router.ts
+++ src/router.ts
@@ -5,10 +5,18 @@
 
 import express from 'express';
 
+const Path = 'api';
+const Version = 'v1';
+
 // exportamos los módulos
 export default (app: express.Express) => {
   // indicamos que para ruta quien la debe resolver
   app.get('/', (req, res) => {
     res.status(200).send('¡Hola BackEnd en TypeScript/Node.js!');
+  });
+
+  // Hola API
+  app.get(`/${Path}/${Version}/hola`, (req, res) => {
+    res.status(200).send(`¡Hola API!: version ${Version}`);
   });
 };
--- tests/server.test.ts
+++ tests/server.test.ts
@@ -9,6 +9,8 @@ process.env.NODE_ENV = 'test';
  */
 describe('Suite Test de Servidor', () => {
   let servidor: any;
+  const Path = 'api';
+  const Version = 'v1';
 
   // instanciamos el servidor
   beforeAll(async () => {
@@ -30,6 +32,13 @@ describe('Suite Test de Servidor', () => {
   test('Debería responder en el metodo GET /', async () => {
     const salida = '¡Hola BackEnd en TypeScript/Node.js!';
     const response = await request(servidor).get('/');
+    expect(response.status).toBe(200);
+    expect(response.text).toBe(salida);
+  });
+
+  test(`Debería responder en el metodo GET /${Path}/${Version}/hola`, async () => {
+    const salida = `¡Hola API!: version ${Version}`;
+    const response = await request(servidor).get(`/${Path}/${Version}/hola`);
     expect(response.status).toBe(200);
     expect(response.text).toBe(salida);
   });
